adee97c8bf77c2f8bb435d0ba9b259d72e3accc4
martin-99 <martinm199913@gmail.com>
2020-03-04 15:30:48 +0000

    Implementing view Customers

--- src/sample/Controller/ViewCustomersController.java
+++ src/sample/Controller/ViewCustomersController.java
@@ -1,4 +1,52 @@
 package sample.Controller;
 
-public class ViewCustomersController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.Customer;
+import sample.SQLiteConnection;
+
+public class ViewCustomersController implements Initializable {
+    @FXML
+    TableView<Customer> customerTable;
+    TableColumn<Customer,Integer> customerId;
+    TableColumn<Customer,String> customerName;
+    TableColumn<Customer,String> customerSurname;
+    TableColumn<Customer,String> customerPhone;
+    TableColumn<Customer,String> customerEmail;
+    ObservableList<Customer> customers = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            Connection connection = SQLiteConnection.Connector();
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM  student;");
+            while (resultSet.next()){
+                customers.add(new Customer(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("surname"),resultSet.getString("phone"),resultSet.getString("email")));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        customerId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        customerName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        customerSurname.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        customerPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        customerEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        customerTable.setItems(customers);
+
+
+
+
+    }
 }
---
+++ src/sample/Customer.java
@@ -0,0 +1,34 @@
+package sample;
+
+public class Customer {
+    private String name,surname,phone,email;
+    private Integer id;
+
+    public Customer(Integer id, String name, String surname, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
